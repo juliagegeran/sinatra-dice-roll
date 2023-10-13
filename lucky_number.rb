3c4082945d19d64f46ae9f3c82e309a4ddb4e453
Julia Ran <59769618+juliagegeran@users.noreply.github.com>
2023-10-13 21:58:17 +0000

    complete

--- lucky_number.rb
+++ lucky_number.rb
@@ -7,66 +7,63 @@ require "binding_of_caller"
 use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
-get("/howdy") do
-  return "Hello, world!"
-end
 
+#set homepage
 get("/") do
-  lucky_num = rand(100)
-
-  return("Your lucky number for today is " + lucky_num.to_s)
+  erb(:elephant)
 end
 
-get("/zebra") do
-  "We must add a route for each path we want to support"
-end
-
-get("/giraffe") do
-  "Hopefully this shows up without having to restart the server 🤞🏾"
-end
-
+#roll two six-sided dice
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+  @roll = []
+  2.times do
+    die = rand(1..6)
+    @roll.push(die)
+  end
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a totle of #{sum}."
-
-  "<h1>2d6</h1>
-  <p>#{outcome}</p>"
+  erb(:two_six)
 end
-
 
 get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
+  #create an empty roll array
+  @roll = []
+  2.times do
+    die = rand(1..10)
+    @roll.push(die)
+  end
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a totle of #{sum}."
-
-  "<h1> 2d10 </h1>
-  <p> #{outcome} </p>"
+  erb(:two_ten)
 end
 
 get("/dice/1/20") do
-  die = rand(1..20)
-  outcome = "You rolled a #{die}."
-  "<h1> 1d20 </h1>
-  <p> #{outcome} </p>"
+  @roll = rand(1..20)  
+  
+  erb(:one_twenty)
+  
 end
-
 
 get("/dice/5/4") do
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
-  
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
-
-  outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die}. The total is #{sum}."
-
-  "<h1> 5d4 </h1>
-  <p> #{outcome} </p>"
+  @roll = []
+  5.times do
+    die = rand(1..4)
+    @roll.push(die)
+  end
+  erb(:five_four)
 end
+
+
+get("/dice/100/6") do
+  #create an empty array with rolls
+  @roll = []
+  
+  #roll a 6-sided die 100 times
+  100.times do
+    die = rand(1..6)
+
+    @roll.push(die)
+
+  end
+  #change the view
+
+  erb(:one_hundred_six)
+end 
